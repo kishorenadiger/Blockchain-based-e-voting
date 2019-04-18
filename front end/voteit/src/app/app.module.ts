3b101950369dbac55be8966e2a57ef383dceee2d
Kishore Nadiger <kishore.nadiger@gmail.com>
2019-04-19 02:10:25 +0530

    front end home page

--- front end/voteit/src/app/app.module.ts
+++ front end/voteit/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BodyComponent } from './components/body/body.component';
+import { FootComponent } from './components/foot/foot.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     BodyComponent,
+    FootComponent,
   ],
   imports: [
     BrowserModule,
